fd56fdbbc8292ed07a5d47dcea596f844bec00c9
Morgana Ribeiro <morgana.ribeiro@globalhitss.com.br>
2022-01-03 11:45:14 -0300

    fix(datasources): adjust data return

--- src/datasources.js
+++ src/datasources.js
@@ -1,31 +1,19 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
 
 class CepPromise extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'http://api.postmon.com.br/v1/cep/';
+    }
+
     async queryAdress(cep) {
-        const response = await this.get(`http://api.postmon.com.br/v1/cep/${cep}`);
-
-        if(!cep){
-            return {
-                error: 'CEP inválido'
-            }
-        }
-        
-        if(!response.ok) {
-            return {
-                error: 'Error ao pesquisar CEP',
-            };
-        }
-
-        const responseJson = await response.json();
-        console.log({responseJson});
-
-        // return {
-        //     cep: responseJson.cep,
-        //     bairro: responseJson.bairro,
-        //     cidade: responseJson.localidade,
-        //     endereco: responseJson.logradouro,
-        //     uf: responseJson.estado,
-        // }
+        const response = await this.get(`${cep}`);
+        return {
+            bairro: response.bairro,
+            cidade: response.cidade,
+            endereco: response.logradouro,
+            uf: response.estado,
+        };       
     }
 }
 
